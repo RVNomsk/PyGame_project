5adaac92176176c39d5ee751873f51808c107f32
rvn <rvnomsk@mail.ru>
2024-03-06 22:50:55 +0300

    Создал класс танк и основновной игровой цикл, настроил управление танком.

---
+++ Second_year_learning/PyGame ПРОЕКТ/main.py
@@ -0,0 +1,71 @@
+import pygame
+# подключаем класс танков
+from tank import Tank
+
+# инициализация
+pygame.init()
+
+# ширина, высота экрана
+WIDTH, HEIGHT = 800, 600
+# частота обновления кадров в секунду
+FPS = 60
+# цвет экрана
+SCREEN_COLOR = (0, 0, 0)
+
+# создадим игровое окно
+screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
+# создадим экземпляр класса Clock для учета времени
+clock = pygame.time.Clock()
+
+# создадим список всех объектов игры
+objects = []
+tank1 = Tank(color="blue",
+             px=100,
+             py=130,
+             direct=0,
+             key_list=(pygame.K_LEFT,
+                       pygame.K_RIGHT,
+                       pygame.K_UP,
+                       pygame.K_DOWN,
+                       pygame.K_KP_ENTER))
+
+tank2 = Tank(color="red",
+             px=600,
+             py=130,
+             direct=0,
+             key_list=(pygame.K_a,
+                       pygame.K_d,
+                       pygame.K_w,
+                       pygame.K_s,
+                       pygame.KMOD_SHIFT))
+objects += [tank1, tank2]
+
+# основной игровой цикл
+running = True
+while running:
+    # перебираем все события в основновном цикле
+    for event in pygame.event.get():
+        # если начать на закрытие окна
+        if event.type == pygame.QUIT:
+            play = False
+
+    # перехват кода нажатой кнопки
+    keys = pygame.key.get_pressed()
+
+    # перебираем список всех объектов игры
+    for obj in objects:
+        obj.update(keys)
+
+    # отрисовка экрана на каждой итерации
+    screen.fill(SCREEN_COLOR)
+
+    # отрисовываем все объекты
+    for obj in objects:
+        obj.draw(screen)
+
+    pygame.display.update()
+    # будем применять двойную буферизацию
+    pygame.display.flip()
+    # метод возвращает количество миллисекунд, прошедших с момента последнего вызова
+    clock.tick(FPS)
+pygame.quit()
---
+++ Second_year_learning/PyGame ПРОЕКТ/tank.py
@@ -0,0 +1,54 @@
+import pygame
+
+# размер картинки в пикселях
+SIZE = 32
+# направления по оси х и y
+DIRECTS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+
+class Tank:
+    def __init__(self, color, px, py, direct, key_list):
+        # тип
+        self.type = "tank"
+        # цвет
+        self.color = color
+        # позиция
+        self.rect = pygame.Rect(px, py, SIZE, SIZE)
+        # направление
+        self.direct = direct
+        # скорость танка по умолчанию
+        self.move_speed = 2
+
+        # кнопки (влево/вправо/вверх/вниз/выстрел)
+        self.key_left = key_list[0]
+        self.key_right = key_list[1]
+        self.key_up = key_list[2]
+        self.key_down = key_list[3]
+        self.key_shot = key_list[4]
+
+    # обновление
+    def update(self, keys):
+        if keys[self.key_left]:
+            self.rect.x -= self.move_speed
+            self.direct = 0
+        elif keys[self.key_right]:
+            self.rect.x += self.move_speed
+            self.direct = 1
+        elif keys[self.key_up]:
+            self.rect.y -= self.move_speed
+            self.direct = 2
+        elif keys[self.key_down]:
+            self.rect.y += self.move_speed
+            self.direct = 3
+        elif keys[self.key_shot]:
+            pass
+
+    # метод отрисовки на экране, экран передаем извне
+    def draw(self, screen):
+        pygame.draw.rect(surface=screen, color=self.color, rect=self.rect)
+
+        x, width = self.rect.centerx, DIRECTS[self.direct][0] * 30
+        y, height = self.rect.centery, DIRECTS[self.direct][1] * 30
+        pygame.draw.line(surface=screen, color="white",
+                         start_pos=(x, y), end_pos=(x + width, y + height),
+                         width=2)
